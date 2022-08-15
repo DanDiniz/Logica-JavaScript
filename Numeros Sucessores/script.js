61c7cc704e6f1cba0a478b11b501ea9cd2201860
Daniel S.Diniz <dan.das.diniz@gmail.com>
2022-08-15 11:50:38 -0300

    Novo exercicio

---
+++ Numeros Sucessores/script.js
@@ -0,0 +1,40 @@
+
+/*Objetivo: Dado um Número inteiro, O algoritimo deve
+  mostrar seus sucessores definidos pelo usuário
+*/ 
+
+var resultados = document.querySelector('.resultado')
+var num = document.getElementById('num');
+var sucessores = document.getElementById('sucessores');
+var button = document.getElementById('button')
+
+
+button.addEventListener('click',()=>{
+
+        let Numero = parseInt(num.value) + 1;
+        let Numero_sucessor = parseInt(sucessores.value) ;
+        let Indice = 0;
+
+
+        if(Numero_sucessor > 10){
+
+          alert("Limite de 10 sucessores")
+
+        }
+        else{
+
+          while(Indice < Numero_sucessor){
+
+            let Soma = Numero ++;
+    
+            resultados.innerHTML+=`
+              <p>`+Indice+` - `+Soma+`</p>
+            `
+        
+        Indice ++;
+    };
+
+  }
+
+    
+});
